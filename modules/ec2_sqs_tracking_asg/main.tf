e04fee67417c8537f561fd35124f17fd4a2c2fb0
yjyjyjy <prowessyang@gmail.com>
2023-06-12 15:42:20 -0700

    larger disk

--- modules/ec2_sqs_tracking_asg/main.tf
+++ modules/ec2_sqs_tracking_asg/main.tf
@@ -67,10 +67,10 @@ resource "aws_launch_template" "default" {
     # }
   }
   block_device_mappings {
-    device_name = "/dev/sda1"
-
+    device_name = "/dev/xvda"
     ebs {
       volume_size = 80
+      delete_on_termination = true
     }
   }
 
