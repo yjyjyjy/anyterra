498b924da7bbe59b9807efd35050fef9f0dcdca8
Oscar <fu.wire@gmail.com>
2023-06-16 11:53:41 -0600

    fix conflicts

--- modules/ec2_sqs_tracking_asg/main.tf
+++ modules/ec2_sqs_tracking_asg/main.tf
@@ -56,7 +56,7 @@ resource "aws_security_group" "default" {
 
 resource "aws_launch_template" "default" {
   name_prefix   = local.launch_type_name_prefix
-  instance_type = var.instance_types_priority_order[1]
+  instance_type = var.instance_types_priority_order[0]
   image_id      = var.image_id
   key_name      = var.access_key_name
   user_data     = base64encode(data.template_file.asg_user_data.rendered)
